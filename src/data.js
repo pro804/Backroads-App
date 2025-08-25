d60b37110242634e519ab034a349b6a03ae39f08
pro804 <petsoulis.appdev@gmail.com>
2025-08-25 20:49:25 +0300

    refactor: implement dynamic Footer component with shared data - Reuse pageLinks and socialLinks data which used in Navbar in Footer component - Replace hardcoded content with dynamic data mapping - Implement dynamic copyright date using JavaScript Date object - Update Font Awesome to latest version in index.html - Replace Twitter icon with X icon in social links data - Improve consistency: changes to Data can now handle from one data file for Navbar and Footer - Enhance maintainability through data sharing between components

--- src/data.js
+++ src/data.js
@@ -14,7 +14,7 @@ export const pageLinks = [
 
 export const socialLinks = [
   { id: 1, href: "https://www.facebook.com", icon: "fab fa-facebook" },
-  { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 2, href: "https://www.twitter.com", icon: "fa-brands fa-x-twitter" },
   { id: 3, href: "https://www.squarespace.com", icon: "fab fa-squarespace" },
 ];
 
